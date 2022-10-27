1b440893736671918be4b935fde08c19a28bf371
SimeonBauman <96099765+SimeonBauman@users.noreply.github.com>
2022-10-27 10:12:53 -0500

    Update HW 6 CSC 200.cpp

--- HW 6 CSC 200.cpp
+++ HW 6 CSC 200.cpp
@@ -8,7 +8,7 @@ int roll(int min, int max);
 int main()
 {
 	srand(time(NULL));
-	int turn = roll(0,1);
+	int turn = roll(0, 1);
 	int Hp1 = roll(15, 25);
 	int Hp2 = roll(15, 25);
 	int A1 = roll(8, 16);
@@ -19,16 +19,17 @@ int main()
 	cout << "Press enter to start the match" << endl;
 	cin.ignore();
 
-	while (Hp1 > 0 && Hp2 > 0) 
+	while (Hp1 > 0 && Hp2 > 0)
 	{
 		if (turn == 0) {
 			cout << "Player 1's turn" << endl;
 
-			if (roll(1, 20) >= A2) 
+			if (roll(1, 20) >= A2)
 			{
 				int damage = roll(2, 6);
 				Hp2 -= damage;
 				cout << "Player 1 did " << damage << " damage" << endl;
+				cout << "Player 2 has " << Hp2 << " health" << endl;
 			}
 			else
 			{
@@ -36,7 +37,7 @@ int main()
 			}
 			turn = 1;
 		}
-		else 
+		else
 		{
 
 			cout << "Player 2's turn" << endl;
@@ -46,17 +47,18 @@ int main()
 				int damage = roll(2, 6);
 				Hp1 -= damage;
 				cout << "Player 2 did " << damage << " damage" << endl;
+				cout << "Player 1 has " << Hp1 << " health" << endl;
 			}
-			else 
+			else
 			{
 				cout << "Player 2 missed" << endl;
 			}
 			turn = 0;
 		}
-		
+
 	}
 
-	if (Hp1 > 0) 
+	if (Hp1 > 0)
 	{
 		cout << "Player 1 Won!" << endl;
 		cout << "Player 2 lost";
@@ -66,13 +68,11 @@ int main()
 		cout << "Player 2 Won!" << endl;
 		cout << "Player 1 lost";
 	}
-    
+
 }
 
 
 int roll(int min, int max)
 {
-	return( rand() % (max - min +1) + min );
+	return(rand() % (max - min + 1) + min);
 }
-
-
